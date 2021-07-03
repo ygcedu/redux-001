dfea651b89a901483c1a1a643b6f97ec6016d7e2
ygc <937601471@qq.com>
2021-07-03 23:01:17 +0800

    重构：useEffect写法整理

--- src/redux.js
+++ src/redux.js
@@ -51,18 +51,14 @@ export const connect = (selector) => (Component) => {
         const [, update] = useState({})
         // 如果selector参数赋值了，则执行selector函数，如果没有则还用全局的state
         const data = selector ? selector(state) : {state};
-        useEffect(() => {
-            const unsubscribe = store.subscribe(() => {
-                const newData = selector ? selector(store.state) : {state: store.state};
-                if (changed(data, newData)) {
-                    console.log('update')
-                    // 传一个新的对象{}, 是什么无所谓，这里只要地址变了就行
-                    update({});
-                }
-            })
-            // 注意这里最好 取消订阅，否则在selector变化时会出现重复订阅
-            return unsubscribe;
-        }, [selector])
+        useEffect(() => store.subscribe(() => {
+            const newData = selector ? selector(store.state) : {state: store.state};
+            if (changed(data, newData)) {
+                console.log('update')
+                // 传一个新的对象{}, 是什么无所谓，这里只要地址变了就行
+                update({});
+            }
+        }), [selector])
         const dispatch = (action) => {
             setState(reducer(state, action));
         }
