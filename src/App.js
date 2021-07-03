de563581b4260bbd2dba2eb214e179c8a1dcf803
ygc <937601471@qq.com>
2021-07-03 21:55:21 +0800

    在 Wrapper 封装组件里让使用了dispatch的组件 UI 能够更新
    存在新问题：没有使用 connect 构建的组件没法更新 UI

--- src/App.js
+++ src/App.js
@@ -57,8 +57,12 @@ const reducer = (state, {type, payload}) => {
 const connect = (Component) => {
     return (props) => {
         const {state, setState} = useContext(appContext);
+        // 这里只需要写数据，用于通知React更新UI
+        const [, update] = useState({})
         const dispatch = (action) => {
             setState(reducer(state, action));
+            // 传一个新的对象{}, 是什么无所谓，这里只要地址变了就行
+            update({})
         }
 
         return <Component {...props} dispatch={dispatch} state={state}/>
