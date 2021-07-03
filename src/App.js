38d70bdd976dcb087cdf05f8db70460105355ff5
ygc <937601471@qq.com>
2021-07-03 22:04:44 +0800

    使用 store.subscribe 来订阅数据变化

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext, useMemo} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 
 const appContext = React.createContext(null)
 
@@ -6,8 +6,17 @@ const store = {
     state: {
         user: {name: 'frank', age: 18}
     },
-    setState(newState){
+    setState(newState) {
         store.state = newState
+        store.listeners.map(fn => fn(store.state))
+    },
+    listeners: [],
+    subscribe(fn) {
+        store.listeners.push(fn);
+        return () => {
+            const index = store.listeners.indexOf(fn)
+            store.listeners.splice(index, 1)
+        }
     }
 }
 
@@ -59,10 +68,14 @@ const connect = (Component) => {
         const {state, setState} = useContext(appContext);
         // 这里只需要写数据，用于通知React更新UI
         const [, update] = useState({})
+        useEffect(() => {
+            store.subscribe(() => {
+                // 传一个新的对象{}, 是什么无所谓，这里只要地址变了就行
+                update({})
+            })
+        }, [])// 只订阅一次
         const dispatch = (action) => {
             setState(reducer(state, action));
-            // 传一个新的对象{}, 是什么无所谓，这里只要地址变了就行
-            update({})
         }
 
         return <Component {...props} dispatch={dispatch} state={state}/>
