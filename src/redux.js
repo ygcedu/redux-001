592748f6c1e3511b83dee9be266174a629180a9b
ygc <937601471@qq.com>
2021-07-03 22:58:28 +0800

    防护措施：useEffect 里返回取消订阅 unsubscribe
    为了防止[selector]依赖变化（理论上不会变化）导致 useEffect 频繁触发渲染

--- src/redux.js
+++ src/redux.js
@@ -52,7 +52,7 @@ export const connect = (selector) => (Component) => {
         // 如果selector参数赋值了，则执行selector函数，如果没有则还用全局的state
         const data = selector ? selector(state) : {state};
         useEffect(() => {
-            store.subscribe(() => {
+            const unsubscribe = store.subscribe(() => {
                 const newData = selector ? selector(store.state) : {state: store.state};
                 if (changed(data, newData)) {
                     console.log('update')
@@ -61,6 +61,7 @@ export const connect = (selector) => (Component) => {
                 }
             })
             // 注意这里最好 取消订阅，否则在selector变化时会出现重复订阅
+            return unsubscribe;
         }, [selector])
         const dispatch = (action) => {
             setState(reducer(state, action));
