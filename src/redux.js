c249e4b76596d54bcc41cddaae73076a6a4954e7
ygc <937601471@qq.com>
2021-07-04 00:35:12 +0800

    redux 重构保持与 redux 官方 store 用法一致

--- src/redux.js
+++ src/redux.js
@@ -1,25 +1,38 @@
 import React, {useState, useContext, useEffect} from 'react';
 
+let state = undefined
+let reducer = undefined
+let listeners = []
+
+const setState = (newState) => {
+    state = newState
+    listeners.map(fn => fn(state))
+}
+
 const store = {
-    state: undefined,
-    reducer: undefined,
-    setState(newState) {
-        store.state = newState
-        store.listeners.map(fn => fn(store.state))
+    getState() {
+        return state;
     },
-    listeners: [],
+    dispatch: (action) => {
+        setState(reducer(state, action));
+    },
     subscribe(fn) {
-        store.listeners.push(fn);
+        listeners.push(fn);
         return () => {
-            const index = store.listeners.indexOf(fn)
-            store.listeners.splice(index, 1)
+            const index = listeners.indexOf(fn)
+            listeners.splice(index, 1)
         }
+    },
+    replaceReducer(newReducer) {
+        reducer = newReducer
     }
 }
 
-export const createStore = (reducer, initState) => {
-    store.state = initState
-    store.reducer = reducer
+const dispatch = store.dispatch
+
+export const createStore = (_reducer, initState) => {
+    state = initState
+    reducer = _reducer
     return store;
 }
 
@@ -36,20 +49,14 @@ const changed = (oldState, newState) => {
 
 export const connect = (selector, dispatcherSelector) => (Component) => {
     return (props) => {
-        const dispatch = (action) => {
-            setState(store.reducer(state, action));
-        }
-        // const {state, setState} = useContext(appContext);
-        // 可以改从store拿，不用从useContext拿
-        const {state, setState} = store;
-        // 这里只需要写数据，用于通知React更新UI
-        const [, update] = useState({})
         // 如果selector参数赋值了，则执行selector函数，如果没有则还用全局的state
         const data = selector ? selector(state) : {state};
         const dispatchers = dispatcherSelector ? dispatcherSelector(dispatch) : {dispatch}
+        // 这里只需要写数据，用于通知React更新UI
+        const [, update] = useState({})
         // 订阅store数据，数据一变化就触发更新
         useEffect(() => store.subscribe(() => {
-            const newData = selector ? selector(store.state) : {state: store.state};
+            const newData = selector ? selector(state) : {state};
             if (changed(data, newData)) {
                 console.log('update')
                 // 传一个新的对象{}, 是什么无所谓，这里只要地址变了就行
